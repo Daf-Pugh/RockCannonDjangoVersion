6b5a069b5e2885ecff7a891aec182705adbbde05
DafPugh <dfp24dgq@bangor.ac.uk>
2026-04-02 19:31:59 +0100

    Pins and li now highlight to make it more user friendly

--- forms/static/forms/js/OSMLeaflet2.js
+++ forms/static/forms/js/OSMLeaflet2.js
@@ -9,10 +9,70 @@ const cannons = JSON.parse(document.getElementById('cannon-data').textContent);
 console.log(typeof cannons);
 console.log(cannons);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
 }).addTo(map);
 
+const normalIcon = L.divIcon({
+  html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+        <path fill="#2f4f4f" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z"/>
+    </svg>`,
+  className: '',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+const highlightIcon = L.divIcon({
+  html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40">
+        <path fill="#DD3006" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z"/>
+    </svg>`,
+  className: '',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -40],
+});
+
+
+var markerMap = {};
+cannons.forEach(cannon => {
+  if (cannon.lat && cannon.lng) {
+    const marker = L.marker([cannon.lat, cannon.lng], { icon: normalIcon })
+      .addTo(map)
+      .bindPopup(cannon.name);
+
+    markerMap[cannon.name] = marker;
+
+    marker.on('mouseover', () => {
+      document.querySelector(`[data-name="${cannon.name}"]`)?.classList.add('highlighted');
+    });
+
+    marker.on('mouseout', () => {
+      document.querySelector(`[data-name="${cannon.name}"]`)?.classList.remove('highlighted');
+    });
+
+  }
+});
+
+document.querySelectorAll('.cannons li').forEach(li => {
+  const name = li.dataset.name;
+
+  li.addEventListener('mouseover', () => {
+    if (markerMap[name]) {
+      markerMap[name].setIcon(highlightIcon);
+    }
+  });
+
+  li.addEventListener('mouseout', () => {
+    if (markerMap[name]) {
+      markerMap[name].setIcon(normalIcon);
+    }
+  });
+});
+
+
+// Aside stuff
 var openIcon = document.getElementById('Arrow-Icon-Menu-Button-Open');
 var closeIcon = document.getElementById('Arrow-Icon-Menu-Button-Close');
 var aside = document.getElementById('Aside-Map');
@@ -31,11 +91,3 @@ function swapIcon() {
   }
 }
 asideButton.addEventListener('click', swapIcon);
-var marker = L.marker([defLat, defLong]).addTo(map);
-cannons.forEach(cannon => {
-  if (cannon.lat && cannon.lng) {
-    L.marker([cannon.lat, cannon.lng])
-      .addTo(map)
-      .bindPopup(cannon.name);
-  }
-});
